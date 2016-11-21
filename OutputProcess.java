41bd5a58453c5a0ed6755747f456fd5ce9ca2b65
Chushlyo <nikolay.p.stoyanov@gmail.com>
2016-11-21 16:19:48 +0100

    Output

--- OutputProcess.java
+++ OutputProcess.java
@@ -4,10 +4,9 @@
 public class OutputProcess {
     void readingInput() {
         int l = scanner.nextLine();
-        ;
         float alpha = scanner.nextLine();
         int m = scanner.nextLine();
-        int taxis = scanner.nextLine();
+        int taxis = scanner.nextLine(); // parse taxis and passengers
         int passengers = scanner.nextLine();
         int destinations = scanner.nextLine();
         int stops[]=new int[destinations];
@@ -17,9 +16,22 @@ public class OutputProcess {
         for (int i = 0; i < destinations; i++) {
             length = scanner.nextInt();
             for (int j=0; j < length; j++ )
-            stops[i] = scanner.nextInt;
-
+            stops[i] = scanner.nextInt();// parse
         }
+
+        int t = scanner.nextInt();
+        int timeCalls = scanner.nextInt();// parse
+
+        length = scanner.nextInt();
+        int calls[][]=new int[999999][2];
+        int counter=0;
+        for (int i=0; i< length; i++);
+        {
+            calls[counter][0] = scanner.nextInt(); //parse
+            calls[counter][1] = scanner.nextInt();
+            counter ++;
+        }
+
 
     }
 }

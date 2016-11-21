f35601e4948da1cad1d67b491d4bbe182291c7d6
Chushlyo <nikolay.p.stoyanov@gmail.com>
2016-11-21 15:56:42 +0100

    Output

---
+++ OutputProcess.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Nikolay on 21-Nov-16.
+ */
+public class OutputProcess {
+    void readingInput() {
+        int l = scanner.nextLine();
+        ;
+        float alpha = scanner.nextLine();
+        int m = scanner.nextLine();
+        int taxis = scanner.nextLine();
+        int passengers = scanner.nextLine();
+        int destinations = scanner.nextLine();
+        int stops[]=new int[destinations];
+        int length;
+
+
+        for (int i = 0; i < destinations; i++) {
+            length = scanner.nextInt();
+            for (int j=0; j < length; j++ )
+            stops[i] = scanner.nextInt;
+
+        }
+
+    }
+}

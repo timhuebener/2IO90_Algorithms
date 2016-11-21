26f7c901b16615478a52584f6a2dfdb0479e71b4
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2016-11-21 14:05:41 +0100

    Addes stuff

--- InputClass.java
+++ InputClass.java
@@ -1,10 +1,15 @@
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.Random;
 
 //Class for generating different inputs
 
 public class InputClass {
 
+ 
+    public TaxiScanner taxi = new TaxiScanner();
+    Scanner scan = new Scanner(System.in);
+    Random rn = new Random();
     
 
 }

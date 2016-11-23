9e22a0ade3f178c16e63ec8c6f7a48ccc4c7f06c
timhuebener <tim.huebener@gmail.com>
2016-11-23 10:58:22 +0100

    updated InputClass

--- InputClass.java
+++ InputClass.java
@@ -10,48 +10,51 @@ public class InputClass {
     private int minutes;
     Random rn = new Random();
     
-    
+    //number of following lines
     private int lineOne(){
-        line5 = (int)(Math.random()*999+1);
+        line5 = rn.nextInt(98)+2;
         return 5 + line5;
     }
+
+    //alpha number
     private float lineTwo(){
         return (float) Math.random();
     }
     
-    
+    //max time that may pass between ordering a taxi and dropping off
     private int lineThree(){
-        return lineFive() * 2;
+        return line5 * 2;
     }
     
-    
+    //number of taxis available and capacity per taxi respectively
     private String lineFour(){
-        return (int)(Math.random()*100+1) + " "+ (int)(Math.random()*15)+1;
-    }
+        return rn.nextInt(line5) + " " + rn.nextInt(15);}
     
-    
+    //number of nodes in graph
     private int lineFive(){
         return line5;
     }
     
-    
+    //connections of node 0
     private String lineSix(){
         String temp = "" + (line5-1);
         for(int i = 1; i < line5; i++){
             temp += " " + i;
         }
         return temp;
-        
     }
-    private void lineSixPlus(){
-        for(int i = 1; i <= line5; i++)
+
+    //connections of rest of nodes
+    private void lineSixPlus() {
+        for (int i = 2; i <= line5; i++) {
             System.out.println("1 0");
+        }
     }
-    
-    
+
+    //Training time and length of caller list
     private String lineSeven(){
-        int temp = (int)(Math.random()*50);
-        minutes =  (int)(Math.random()*100+temp);
+        int temp = rn.nextInt(10);
+        minutes =  rn.nextInt(100)+temp;
         
         
         return temp + " "+ minutes;
@@ -60,11 +63,11 @@ public class InputClass {
     
     private void lineSevenPlus(){
         for(int i = 0; i < minutes; i++){
-            int temp = (int)(Math.random()*100);
+            int temp = rn.nextInt(40);
             System.out.print(temp+" ");
             for(int j = 0; j < temp; j++){
-                int start = (int)(Math.random() * (line5+1));
-                int end = (int)(Math.random() * (line5+1));
+                int start = rn.nextInt(line5);
+                int end = rn.nextInt(line5);
                 if(start == end)
                     j--;
                 else{
@@ -73,22 +76,29 @@ public class InputClass {
             }
             System.out.println();
         }
-        
-        
     }
     
     
     public static void main(String[] args){
         
         InputClass temp = new InputClass();
+        //System.out.println("Number of lines");
         System.out.println(temp.lineOne());
+        //System.out.println("Alpha number");
         System.out.println(temp.lineTwo());
+        //System.out.println("Max time collect drop off");
         System.out.println(temp.lineThree());
+        //System.out.println("Number of Taxis and Capacity");
         System.out.println(temp.lineFour());
+        //System.out.println("Number of nodes");
         System.out.println(temp.lineFive());
+        //System.out.println("Node 0 connections");
         System.out.println(temp.lineSix());
+        //System.out.println("Rest of connections");
         temp.lineSixPlus();
+        //System.out.println("Training time and length of caller list");
         System.out.println(temp.lineSeven());
+        //System.out.println("Input");
         temp.lineSevenPlus();
     }
 }

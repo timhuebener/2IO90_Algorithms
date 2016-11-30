0369f87ab2ba4598a99cb00fe2a85d4cee4f4e0c
WouterSchols <wouter.schols@hotmail.com>
2016-11-30 14:26:33 +0100

    input class

--- InputClass.java
+++ InputClass.java
@@ -1,106 +1,355 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
 
 
 //Class for generating different inputs
 
 
 public class InputClass {
-    private int line5;
-    private int minutes;
-    Random rn = new Random();
-
-    //number of following lines
-    private int lineOne(){
-        line5 = rn.nextInt(8)+2;
-        return 5 + line5;
-    }
-
-    //alpha number
-    private float lineTwo(){
-        return (float) Math.random();
-    }
-
-    //max time that may pass between ordering a taxi and dropping off
-    private int lineThree(){
-        return line5 * 2;
-    }
-
-    //number of taxis available and capacity per taxi respectively
-    private String lineFour(){
-        return rn.nextInt(line5) + " " + rn.nextInt(15);}
-
-    //number of nodes in graph
-    private int lineFive(){
-        return line5;
-    }
-
-    //connections of node 0
-    private String lineSix(){
-        String temp = "" + (line5-1);
-        for(int i = 1; i < line5; i++){
-            temp += " " + i;
-        }
-        return temp;
-    }
-
-    //connections of rest of nodes
-    private void lineSixPlus() {
-        for (int i = 2; i <= line5; i++) {
-            System.out.println("1 0");
-        }
-    }
-
-    //Training time and length of caller list
-    private String lineSeven(){
-        int temp = rn.nextInt(10);
-        minutes =  rn.nextInt(10)+temp;
+	private int line5;
+	private int minutes;
+	Random rn = new Random();
+	static Scanner scanner = new Scanner(System.in);
+	float alpha;
+	int maxTime;
+	int amTaxi;
+	int sizeTaxi;
+	int amNodes;
+	int amclusters;
+	float conNodes;
+	float conClusters;
+	float link;
+	int training;
+	int endday = 0;
+	int type;
+	int start;
+	int end;
+	double density;
+	ArrayList<Integer> customerstype = new ArrayList<Integer>();
+	ArrayList<Integer> customersstart = new ArrayList<Integer>();
+	ArrayList<Integer> customersend = new ArrayList<Integer>();
+	ArrayList<Integer> clustersarray = new ArrayList<Integer>();
+	ArrayList<Double> customersdensity = new ArrayList<Double>();
 
 
-        return temp + " "+ minutes;
-    }
+
+	public static void main(String[] args){
+		InputClass temp = new InputClass();
+		System.out.println("Alpha, double between 0 and 1"); 
+		temp.alpha = scanner.nextFloat();
+		System.out.println("Maximum time allowed for one person, int larger than 0"); 
+		temp.maxTime = scanner.nextInt();
+		System.out.println("Amount of taxis, int larger than 0"); 
+		temp.amTaxi = scanner.nextInt();
+		System.out.println("Maximum amount of people transported by a taxi, int larger than 0"); 
+		temp.sizeTaxi = scanner.nextInt();
+		System.out.println("Amount of nodes, int larger than 0"); 
+		temp.amNodes = scanner.nextInt();
+		System.out.println("amount of clusters, int larger than 0 smaller than half of the amount of nodes");
+		temp.amclusters = scanner.nextInt();
+		System.out.println("percentage possible edges in clusters, double between 0 and 1"); 
+		temp.conNodes = scanner.nextFloat();
+		System.out.println("percentage connected clusters , double between 0 and 1");
+		temp.conClusters = scanner.nextFloat();
+		System.out.println("strength link connected clusters , double between 0 and 1"); 
+		temp.link = scanner.nextFloat();
+		System.out.println("generate customers:type, start time(int>=0), end time(int>0), average(bouble>0)");
+		System.out.println("1 = close customers");
+		System.out.println("2 = far customers");
+		System.out.println("3 = random customers");
+		System.out.println("4 = end day");
+		while(temp.type != 4){
+			temp.type = scanner.nextInt();
+			if(temp.type!=4){
+			temp.start = scanner.nextInt();
+			temp.end = scanner.nextInt();
+			temp.density = 1/(scanner.nextDouble());
+			if(temp.end>temp.endday){
+				temp.endday = temp.end;
+			}
+			temp.customerstype.add(temp.type);
+			temp.customersstart.add(temp.start);
+			temp.customersend.add(temp.end);
+			temp.customersdensity.add(temp.density);
+			}
+		}
+		
+		System.out.println("End training periode, int larger than 0");
+		temp.training = scanner.nextInt();
+		
+		
+		
 
 
-    private void lineSevenPlus(){
-        for(int i = 0; i < minutes; i++){
-            int temp = rn.nextInt(6);
-            System.out.print(temp+" ");
-            for(int j = 0; j < temp; j++){
-                int start = rn.nextInt(line5);
-                int end = rn.nextInt(line5);
-                if(start == end)
-                    j--;
-                else{
-                    System.out.print(start + " " + end + " ");
-                }
-            }
-            System.out.println();
-        }
-    }
 
-    public void createInput(){
-        InputClass temp = new InputClass();
-        //System.out.println("Number of lines");
-        System.out.println(temp.lineOne());
-        //System.out.println("Alpha number");
-        System.out.println(temp.lineTwo());
-        //System.out.println("Max time collect drop off");
-        System.out.println(temp.lineThree());
-        //System.out.println("Number of Taxis and Capacity");
-        System.out.println(temp.lineFour());
-        //System.out.println("Number of nodes");
-        System.out.println(temp.lineFive());
-        //System.out.println("Node 0 connections");
-        System.out.println(temp.lineSix());
-        //System.out.println("Rest of connections");
-        temp.lineSixPlus();
-        //System.out.println("Training time and length of caller list");
-        System.out.println(temp.lineSeven());
-        //System.out.println("Input");
-        temp.lineSevenPlus();
-    }
 
-    public static void main(String[] args){
-        new InputClass().createInput();
-    }
+
+
+
+
+
+		try {
+			System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream("C:\\Users\\s157937\\Documents\\Jaar_2_Sofware_sience\\q2\\algorithms\\input.txt")), true));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(temp.lineOne());
+		System.out.println(temp.lineTwo());
+		System.out.println(temp.lineThree());
+		System.out.println(temp.lineFour());
+		System.out.println(temp.lineFive());
+		temp.simGraph();
+		System.out.println(temp.linelast());
+		temp.simPeople();
+		
+
+	}
+
+
+	private int lineOne(){
+		return 5 + amNodes;
+	}
+
+	private double lineTwo(){
+		return alpha;
+	}
+
+
+	private int lineThree(){
+		return maxTime;
+	}
+
+
+	private String lineFour(){
+		String temp = amTaxi + " " + sizeTaxi;
+		return temp;
+	}
+
+
+	private int lineFive(){
+		return amNodes;
+	}
+
+
+	private String linelast(){
+		String temp = training+" "+endday;
+		return temp;
+	}
+
+	
+	void simGraph(){
+		int[] clusters = new int[amclusters];
+		int[] counter = new int[amNodes];
+		String[] node = new String[amNodes];
+		for(int i = 0; i < amNodes;i++){
+			node[i]="";
+			counter[i]=0;
+		}
+		for(int i = 0; i < amclusters;i++){
+			clusters[i]=2;
+		}
+
+		for(int i=2*amclusters; i<amNodes;i++){
+			clusters[rn.nextInt(amclusters)]++;
+		}
+
+
+		int temp = 0;
+
+		for(int k=0; k<amclusters;k++){//make all clusters
+
+			int temph=temp+1;
+			node[temph] += " " + temp;
+			node[temp] += " "+temph;
+			counter[temph]++;
+			counter[temp]++;
+			for(int i = 2; i < clusters[k]; i++){//connect graph
+				int tempr = rn.nextInt(i-1);
+				int store1 = i+ temp;
+				int store2 = temp+tempr;
+				node[store1] += " " + store2;
+				node[store2] += " "+store1;
+				counter[store1]++;
+				counter[store2]++;
+				for(int z = temp; z < store1; z++){//randomly add extra nodes
+					double tempr2 = Math.random();
+					if(tempr2<conNodes && store2 != z ){
+						node[store1] += " " + z;
+						node[z] += " "+store1;
+						counter[store1]++;
+						counter[z]++;
+					}
+				}
+			}
+
+			temp+= clusters[k];
+		}
+		if(amclusters>1){
+			int conclusters[][]=new int[amclusters][amclusters];
+			for(int i = 0; i < amclusters; i++){//finalize links
+				for(int z = 0; z < amclusters; z++){//randomly add extra nodes
+					conclusters[i][z]=0;	
+			}
+		}
+			conclusters[0][1]=1;
+			conclusters[1][0]=1;
+			for(int i = 2; i < amclusters; i++){//connect clusters
+				int randin = rn.nextInt(i-1);
+				conclusters[i][randin]++;
+				conclusters[randin][i]++;
+				for(int z = 0; z < i; z++){//randomly add extra nodes
+					double tempr2 = Math.random();
+					if(tempr2<conClusters && conclusters[i][z] == 0 ){
+						conclusters[i][z]++;
+						conclusters[z][i]++;
+					}
+					if(clusters[z]<clusters[i]&& conclusters[i][z] != 0 ){//make links stronger
+						conclusters[z][i]=Math.round(clusters[z]*link);
+						conclusters[i][z]=conclusters[z][i];
+					}
+					if(clusters[z]>=clusters[i] && conclusters[i][z] != 0 ){
+						conclusters[z][i]=Math.round(clusters[i]*link);
+						conclusters[i][z]=conclusters[z][i];
+					}
+				}
+			}
+			
+			int tempi = 0;
+			int tempz = 0;
+			for(int i = 0; i < amclusters; i++){//finalize links
+				for(int z = 0; z < i; z++){//randomly add extra nodes
+					ArrayList<Integer> listz = new ArrayList<Integer>();
+					for (int y=tempz; y<tempz+clusters[z]; y++) {
+						listz.add(new Integer(y));
+					}
+					ArrayList<Integer> listi = new ArrayList<Integer>();
+					for (int y=tempi; y<tempi+clusters[i]; y++) {
+						listi.add(new Integer(y));
+					}
+					Collections.shuffle(listz);
+					Collections.shuffle(listi);
+					for (int y=0; y<conclusters[z][i]; y++) {
+						node[listi.get(y)] += " " + listz.get(y);
+						node[listz.get(y)] += " "+listi.get(y);
+						counter[listi.get(y)]++;
+						counter[listz.get(y)]++;
+					}
+					tempz+=clusters[z];
+				}
+				tempz = 0;
+				tempi+=clusters[i];
+			}
+		}
+
+		for(int i = 0; i < amNodes;i++){
+			node[i]=counter[i]+node[i];
+			System.out.println(node[i]);
+		}
+		for(int i = 0; i < amclusters;i++){//make clusters public
+			clustersarray.add(clusters[i]);
+		}
+
+	}
+	
+	public static int getPoisson(double lambda) {
+		  double L = Math.exp(-lambda);
+		  double p = 1.0;
+		  int k = 0;
+
+		  do {
+		    k++;
+		    p *= Math.random();
+		  } while (p > L);
+
+		  return k - 1;
+		}
+
+
+	void simPeople(){
+		int realtime[] = new int[endday+1];
+		String realorders[] = new String[endday+1];
+		for(int i=0;i<endday+1;i++){
+			realtime[i]=0;
+			realorders[i]="";
+		}
+		int time;
+		for(int i = 0; i < customerstype.size(); i++){
+			time = customersstart.get(i);
+			while( time < customersend.get(i)){
+				time +=getPoisson(customersdensity.get(i));
+				if(time<endday){
+				realtime[Math.round(time)]++;
+				if(customerstype.get(i)==1){
+					realorders[Math.round(time)]+=customer1();
+				}
+				if(customerstype.get(i)==2){
+					realorders[Math.round(time)]+=customer2();
+				}
+				if(customerstype.get(i)==3){
+					realorders[Math.round(time)]+=customer3();
+				}
+			}
+			}
+		}
+		for(int i=0;i<endday+1;i++){
+			System.out.println(realtime[i]+realorders[i]);
+		}
+	}
+	
+	String customer1(){
+		int nodestart=0;
+		int nodeend;
+		String result;
+		int cluster1 = 0;
+		if(amclusters!=1){
+			cluster1 = rn.nextInt(amclusters-1);
+		}
+		for(int i=0; i < cluster1; i++){
+			nodestart+=clustersarray.get(i);
+		}
+		nodeend=nodestart;
+		nodeend+=rn.nextInt(clustersarray.get(cluster1));
+		nodestart+=rn.nextInt(clustersarray.get(cluster1));
+		result= " "+nodestart+" "+nodeend;
+		return result;
+	}
+	String customer2(){
+		int nodestart=0;
+		int nodeend=0;
+		int cluster2;
+		String result;
+		int cluster1 = rn.nextInt(amclusters-1);
+		cluster2=cluster1;
+		while(cluster2!=cluster1){
+		cluster2 = rn.nextInt(amclusters-1);
+		}
+		for(int i=0; i < cluster1; i++){
+			nodestart+=clustersarray.get(i);
+		}
+		for(int i=0; i < cluster2; i++){
+			nodeend+=clustersarray.get(i);
+		}
+		nodeend+=rn.nextInt(clustersarray.get(cluster2));
+		nodestart+=rn.nextInt(clustersarray.get(cluster1));
+		result= " "+nodestart+" "+nodeend;
+		return result;
+	}
+	String customer3(){
+		String result = " "+ rn.nextInt(amclusters-1) +" "+ rn.nextInt(amclusters-1);
+		return result;
+	}
+
+
+
+
 }

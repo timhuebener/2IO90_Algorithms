bf37ce66831441f6d89efdbbe72610c2d190ebad
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2017-01-11 10:36:51 +0100

    Commented out the running time print

--- Algorithm.java
+++ Algorithm.java
@@ -219,8 +219,8 @@ public class Algorithm {
             scanner.println(line + "c");// end minute
             line = "";
         }
-        System.out.println(((double) (System.nanoTime() - time) / 1000000000.0)
-                           + " ");
+       // System.out.println(((double) (System.nanoTime() - time) / 1000000000.0)
+         //                  + " ");
         //efficiency();
     }
     
